d5b82fc39df16f8a8d179603e790c728d1f15351
Fletcher De Meo <fletcher.demeo@gmail.com>
2022-12-08 07:23:13 +0800

    mortgage calculator

---
+++ lesson_2/mortgage_calculator.js
@@ -0,0 +1,53 @@
+const readline = require('readline-sync');
+const MESSAGES = require('./mortgage_calculator_messages.json');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number, noZeros=false) {
+  let test = number.trimStart() === '' || Number.isNaN(Number(number)); 
+  if (noZeros) {
+    return test || Number(number) === 0;
+  }
+  return test || Number(number) < 0;
+}
+
+// Get loan amount
+prompt(MESSAGES['loanAmount']);
+let loanAmount = readline.question();
+while (invalidNumber(loanAmount)) {
+  prompt(MESSAGES['validNumber']);
+  loanAmount = readline.question();
+}
+
+// Get annual percentage rate
+prompt(MESSAGES['annualRate']);
+let annualRate = readline.question();
+while (invalidNumber(annualRate)) {
+  prompt(MESSAGES['validNumber']);
+  annualRate = readline.question();
+}
+
+// Get loan length in years
+prompt(MESSAGES['loanDuration']);
+let loanDurationYears = readline.question();
+while (invalidNumber(loanDurationYears, true)) {
+  prompt(MESSAGES['validNumber']);
+  loanDurationYears = readline.question();
+}
+
+// Create calculation variables
+let loanDurationMonths = Number(loanDurationYears) * 12;
+let monthlyRate = (Number(annualRate) / 100) / 12;
+let monthlyPayment;
+if (annualRate === '0') {
+  monthlyPayment = Number(loanAmount) / loanDurationMonths;
+} else {
+  monthlyPayment =
+    Number(loanAmount) *
+    (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDurationMonths))));
+}
+
+// Display monthly payment in dollar and cents format
+prompt(MESSAGES['monthlyPayment'] + monthlyPayment.toFixed(2));
